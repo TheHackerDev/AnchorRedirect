86753b8221a2689265cb7665bd28785b35582ed4
Aaron Hnatiw <aaron@aaronhnatiw.ca>
2013-03-17 13:42:41 -0400

    Commentt added to JavaScript file

--- Extension/anchor_redirect.js
+++ Extension/anchor_redirect.js
@@ -1,6 +1,10 @@
+// Assign all of the anchor elements to an array variable named "links"
 var links = document.getElementsByTagName('a');
+// Loop through all the anchor elements
 for(var i=0; i < links.length; i++){
+    // Change the "onclick" event of the anchor tags to
+    // execute a function where the href element is changed
+    // to reference google.com
     links[i].onclick = function(){
-        this.href = 'http://google.com';  // Send all data to google.com
-    };
+        this.href = 'http://google.com';
 }
